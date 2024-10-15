c260a83bedb2813ac2c4cadf6bbd6497312c1f97
amanSingh35 <143202353+amanSingh35@users.noreply.github.com>
2024-10-16 02:13:58 +0530

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL='http://notes-backend-env.eba-9zimpemt.ap-south-1.elasticbeanstalk.com'
+export const BASE_URL='http://notes-backend-env.eba-9zimpemt.ap-south-1.elasticbeanstalk.com/'
