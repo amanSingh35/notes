6dbc244909be3de87e7caac407eeebe35ed51db6
amanSingh35 <143202353+amanSingh35@users.noreply.github.com>
2024-10-16 20:40:19 +0530

    Update ProfileInfo.jsx

--- frontend/src/components/Cards/ProfileInfo.jsx
+++ frontend/src/components/Cards/ProfileInfo.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { getinitials } from "../../utils/helper";
 
-const 
-ProfileInfo = ({ userInfo, onLogout }) => {
+const ProfileInfo = ({ userInfo, onLogout }) => {
   if (!userInfo) {
-    return null; // or a loading indicator like a spinner
+    return null; // or a loading indicator
   }
 
   return (
-    <div className="flex items-center gap-3">
-      <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
+    <div className="flex items-center gap-2">
+      <div className="w-10 h-10 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
         {getinitials(userInfo.fullName)}
       </div>
-      <div className="">
+      <div className="flex flex-col items-start">
         <p className="text-sm font-medium">{userInfo.fullName}</p>
-        <button className="text-sm text-blue-700 underline" onClick={onLogout}>
+        <button className="text-xs text-blue-700 underline" onClick={onLogout}>
           Logout
         </button>
       </div>
