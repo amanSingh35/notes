cb97760797f6c40904d472a4d7380df5cbcb665a
amanSingh35 <143202353+amanSingh35@users.noreply.github.com>
2024-10-16 20:41:35 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import ProfileInfo from "./Cards/ProfileInfo";
 import SearchBar from "./SearchBar/SearchBar";
 import { useNavigate } from "react-router-dom";
 
-const Navbar = ({onSearchNote,userInfo,handleClearSearch}) => {
+const Navbar = ({ onSearchNote, userInfo, handleClearSearch }) => {
   const [searchQuery, setSearchQuery] = useState("");
 
   const navigate = useNavigate();
@@ -13,28 +13,28 @@ const Navbar = ({onSearchNote,userInfo,handleClearSearch}) => {
     navigate("/login");
   };
 
-  const handleSearch=()=>{
-    if(searchQuery){
+  const handleSearch = () => {
+    if (searchQuery) {
       onSearchNote(searchQuery);
     }
   };
 
-  const onClearSearch=()=>{
+  const onClearSearch = () => {
     setSearchQuery("");
     handleClearSearch();
-  }
+  };
 
   return (
-    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
-      <h2 className="text-xl font-medium text-black py-2">Notes</h2>
-      <SearchBar
-        value={searchQuery}
-        onChange={(e) => {
-          setSearchQuery(e.target.value);
-        }}
-        handleSearch={handleSearch}
-        onClearSearch={onClearSearch}
-      />
+    <div className="bg-white flex items-center justify-between px-4 py-2 drop-shadow-md">
+      <h2 className="text-xl font-medium text-black">Notes</h2>
+      <div className="flex-grow mr-4">
+        <SearchBar
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          handleSearch={handleSearch}
+          onClearSearch={onClearSearch}
+        />
+      </div>
       <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
     </div>
   );
