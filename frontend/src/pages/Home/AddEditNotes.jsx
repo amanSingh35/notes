7ae4e436640798f8dd860fb1750774e815048c61
amanSingh35 <143202353+amanSingh35@users.noreply.github.com>
2024-10-18 04:37:49 +0530

    Update AddEditNotes.jsx

--- frontend/src/pages/Home/AddEditNotes.jsx
+++ frontend/src/pages/Home/AddEditNotes.jsx
@@ -69,49 +69,49 @@ const AddEditNotes = ({ showToastMessage, getAllNotes, noteData, type, onClose }
   };
 
   return (
-    <div className="fixed inset-0 bg-white bg-opacity-90 flex flex-col p-4 md:p-8 overflow-y-auto z-50">
-      <button
-        className="w-10 h-10 rounded-full flex items-center justify-center absolute top-4 right-4 hover:bg-slate-500 transition-colors"
-        onClick={onClose}
-      >
-        <MdClose className="text-xl text-slate-400" />
-      </button>
+    <div className="fixed inset-0 flex items-center justify-center z-50 p-4">
+      <div className="relative bg-white w-full md:w-1/2 p-6 rounded-md shadow-lg max-h-[90vh] overflow-y-auto">
+        <button
+          className="w-10 h-10 rounded-full flex items-center justify-center absolute top-4 right-4 hover:bg-slate-500 transition-colors"
+          onClick={onClose}
+        >
+          <MdClose className="text-xl text-slate-400" />
+        </button>
 
-      {/* Content Wrapper */}
-      <div className="flex flex-col gap-y-2 mt-16 md:mt-4">
-        <label className="input-level">TITLE</label>
-        <input
-          type="text"
-          className="text-lg text-slate-950 outline-none border border-slate-300 rounded p-2"
-          placeholder="Go to gym"
-          value={title}
-          onChange={({ target }) => setTitle(target.value)}
-        />
-      </div>
+        <div className="flex flex-col gap-y-2">
+          <label className="input-level">TITLE</label>
+          <input
+            type="text"
+            className="text-lg text-slate-950 outline-none border border-slate-300 rounded p-2"
+            placeholder="Go to gym"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
 
-      <div className="flex flex-col gap-2 mt-4">
-        <label className="input-label">CONTENT</label>
-        <textarea
-          type="text"
-          className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded border border-slate-300"
-          placeholder="Content"
-          rows={8}
-          value={content}
-          onChange={({ target }) => setContent(target.value)}
-        />
+        <div className="flex flex-col gap-2 mt-4">
+          <label className="input-label">CONTENT</label>
+          <textarea
+            type="text"
+            className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded border border-slate-300"
+            placeholder="Content"
+            rows={8}
+            value={content}
+            onChange={({ target }) => setContent(target.value)}
+          />
+        </div>
+        <div className="mt-3">
+          <label className="input-label">TAGS</label>
+          <TagInput tags={tags} setTags={setTags} />
+        </div>
+        {error && <p className="text-red-500 text-xs pt-4">{error}</p>}
+        <button
+          className="btn_primary font-medium mt-5 p-3"
+          onClick={handleAddNote}
+        >
+          {type === "edit" ? "UPDATE" : "ADD"}
+        </button>
       </div>
-      <div className="mt-3">
-        <label className="input-label">TAGS</label>
-        <TagInput tags={tags} setTags={setTags} />
-      </div>
-      {/* Error message */}
-      {error && <p className="text-red-500 text-xs pt-4">{error}</p>}
-      <button
-        className="btn_primary font-medium mt-5 p-3"
-        onClick={handleAddNote}
-      >
-        {type === "edit" ? "UPDATE" : "ADD"}
-      </button>
     </div>
   );
 };
